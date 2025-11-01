747ef27b2a77c912ef1bffd47105835564f5dc7e
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-11-01 22:10:51 +0500

    updated

--- CollectionsFrameWork.java
+++ CollectionsFrameWork.java
@@ -1,5 +1,20 @@
 public class CollectionsFrameWork {
     public static void main(String[] args) {
-        
+        //Collection of classes and Interfaces
+    // Interface 
+    // Collection 
+    // List queue set 
+
+
+
+    //Methods in DSA
+    // add
+    // size
+    // remove
+    // iterate 
+    // addAll
+    // removeAll
+    // clear
+    
     }
 }
